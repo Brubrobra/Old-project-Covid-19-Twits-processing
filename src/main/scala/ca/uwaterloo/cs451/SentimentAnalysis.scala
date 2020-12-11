2d776d3c714568c52359c3932083630e9ebd4b03
Jiayue Cheng <jiayue.cheng@uwaterloo.ca>
2020-12-11 05:16:42 -0500

    new files

---
+++ src/main/scala/ca/uwaterloo/cs451/SentimentAnalysis.scala
@@ -0,0 +1,96 @@
+/* 
+  input files format: term, count, dates
+  for each term, we get its sentiment value sentiment(term)
+  then set the total sentiment = count*sentiment(term)
+  then we group the results by dates 
+*/
+
+package ca.uwaterloo.cs451.COVID19Predictor
+
+import org.apache.commons.io.FilenameUtils
+import org.apache.log4j._
+import org.apache.hadoop.fs._
+import org.rogach.scallop._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+import java.time.format.DateTimeFormatter
+import ca.uwaterloo.cs451.COVID19Predictor.SentimentAnalysisUtils._
+//import com.cybozu.labs.langdetect.DetectorFactory
+//import com.github.vspiewak.util.LogUtils
+//import com.github.vspiewak.util.SentimentAnalysisUtils._
+import org.apache.spark.SparkConf
+//import org.apache.spark.streaming.twitter._
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+//import org.elasticsearch.spark._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+
+import scala.util.Try
+
+object SentimentAnalysis {
+
+  val log = Logger.getLogger(getClass().getName())
+  
+  class SAConf(args: Seq[String]) extends ScallopConf(args) {
+    mainOptions = Seq(input, output)
+    val input = opt[String](descr = "input path", required = true)
+    val output = opt[String](descr = "output path", required = true)
+    verify()
+  }
+
+  def main(argv: Array[String]){
+    val args = new SAConf(argv)
+    
+    val inputPath = args.input()
+    val outputPath = args.output()
+    val outputDir = new Path(outputPath)
+
+    log.info("Input path: " + inputPath )
+    log.info("Output path: " + outputPath )
+
+    //val sc = SparkSession.builder().appName("SentimentAnalysis").getOrCreate()
+    val conf = new SparkConf().setAppName("SentimentAnalysis")
+    val sc = new SparkContext(conf)
+    FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
+
+    val trigramRdd = sc.textFile(inputPath)
+      .map(line => line.split(","))
+      .filter(tokens => tokens(1) != "counts")// text, count, date
+      .map(tokens => {
+        val date = tokens(2).toString
+        val text = tokens(0).toString
+        val count = tokens(1).toInt
+
+        val sentiment = detectSentiment(text).toDouble * count
+
+        (date, sentiment)
+      })
+        .map(p => (p._1, p._2))
+      .reduceByKey(_+_)
+      .sortByKey()
+      //.map(tokens => (tokens(2).toString, (tokens(0).toString, tokens(1).toString)))
+      //.collect()
+
+    trigramRdd.saveAsTextFile(args.output())
+
+    val individualValuesRdd = sc.textFile(inputPath)
+      .map(line => line.split(","))
+      .filter(tokens => tokens(1) != "counts")
+      .map( tokens => {
+        val date = tokens(2).toString
+        val text = tokens(0).toString
+        val count = tokens(1).toInt
+
+        val sentiment = detectSentiment(text).toDouble
+
+        (date, text, count, sentiment)
+      })
+
+    individualValuesRdd.saveAsTextFile(args.output()+"/singles")
+
+    
+
+  
+  }
+}
